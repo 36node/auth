b40db42511a4b5b8ec88453694d7ad390ef24e24
lotic <lotic1114@gmail.com>
2024-11-04 12:20:57 +0800

    修改redis连接相关已适配单机和集群

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,18 +1,34 @@
-import { Inject, Module } from '@nestjs/common';
-import { createClient, RedisClientType } from 'redis';
+import { Global, Inject, Module } from '@nestjs/common';
+import { createClient, createCluster, RedisClientType } from 'redis';
 
 import * as config from './config';
 
+@Global()
 @Module({
   providers: [
     {
       provide: 'REDIS_CLIENT',
       useFactory: async () => {
-        const client = createClient({
-          url: config.url,
-        });
-        await client.connect();
-        return client;
+        const urls = config.url.split(',');
+        if (urls && urls.length > 1) {
+          const cluster = createCluster({
+            rootNodes: urls.map((node) => {
+              return { url: node };
+            }),
+          });
+          await cluster.connect();
+          console.log(await cluster.get('test'));
+          return cluster;
+        } else if (urls && urls.length > 0) {
+          const client = createClient({
+            url: config.url,
+          });
+
+          await client.connect();
+          console.log(await client.get('test'));
+          return client;
+        }
+        throw new Error('Redis url error, url=' + config.url);
       },
     },
   ],
