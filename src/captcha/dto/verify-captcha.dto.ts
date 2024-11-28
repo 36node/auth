4452e4402e6ac5a59c4182eadd060f3495dab2b6
zzswang <zzswang@gmail.com>
2024-11-28 14:52:21 +0800

    fix: captcha dto

--- src/captcha/dto/verify-captcha.dto.ts
+++ src/captcha/dto/verify-captcha.dto.ts
@@ -1,12 +1,18 @@
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
 export class VerifyCaptchaDto {
   /**
    * 验证码
    */
+  @IsString()
+  @IsNotEmpty()
   code: string;
 
   /**
    * 验证码 key
    */
+  @IsNotEmpty()
+  @IsString()
   key: string;
 }
 
@@ -14,5 +20,6 @@ export class VerifyCaptchaResultDto {
   /**
    * 是否验证成功
    */
+  @IsBoolean()
   success: boolean;
 }
