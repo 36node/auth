ee1e04e65500728273a82141fd127243682ef91d
zzswang <zzswang@gmail.com>
2025-05-13 11:38:07 +0800

    fix: register user with more info

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -4,16 +4,39 @@ import { IsOptional, IsString } from 'class-validator';
 import { UserDoc } from '../entities/user.entity';
 
 export class CreateUserDto extends OmitType(UserDoc, [
+  'groups',
   'labels',
   'lastSeenAt',
   'lastLoginIp',
   'lastLoginAt',
-  'registerIp',
+  'permissions',
+  'roles',
 ] as const) {
+  /**
+   * 团队
+   */
+  @IsOptional()
+  @IsString({ each: true })
+  groups?: string[];
+
   /**
    * 标签
    */
   @IsOptional()
   @IsString({ each: true })
   labels?: string[];
+
+  /**
+   * 权限
+   */
+  @IsOptional()
+  @IsString({ each: true })
+  permissions?: string[];
+
+  /**
+   * 角色
+   */
+  @IsOptional()
+  @IsString({ each: true })
+  roles?: string[];
 }
