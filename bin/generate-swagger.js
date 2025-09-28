044f8a19d3ef7c9c1109b8b52680de0b4a610dc6
zzswang <zzswang@gmail.com>
2025-09-28 21:42:01 +0800

    feat: swagger generator

--- bin/generate-swagger.js
+++ bin/generate-swagger.js
@@ -1,62 +1,16 @@
-const fs = require('fs');
-
 const { NestFactory } = require('@nestjs/core');
-const { SwaggerModule, DocumentBuilder } = require('@nestjs/swagger');
 const commander = require('commander');
 
 const { AppModule } = require('../dist/app.module');
-const { SHA256 } = require('crypto-js');
-const { ListUsersQuery } = require('../dist/user/dto/list-users.dto');
-const { ListNamespacesQuery } = require('../dist/namespace/dto/list-namespaces.dto');
-const { GetAuthorizerQuery } = require('../dist/auth/dto/authorize-query.dto');
-const { ListCaptchasQuery } = require('../dist/captcha/dto/list-captchas.dto');
-const { ListEmailRecordsQuery } = require('../dist/email/dto/list-email-records.dto');
-const { ListGroupsQuery } = require('../dist/group/dto/list-groups.dto');
-const { ListIndustriesQuery } = require('../dist/industry/dto/list-industries.dto');
-const { ListRolesQuery } = require('../dist/role/dto/list-roles.dto');
-const { ListSessionsQuery } = require('../dist/session/dto/list-sessions.dto');
-const { ListSmsRecordsQuery } = require('../dist/sms/dto/list-sms-records.dto');
-const { ListThirdPartyQuery } = require('../dist/third-party/dto/list-third-party.dto');
+const { writeOpenapi } = require('../dist/swagger');
 
+/**
+ * @param {string | undefined} prefix
+ */
 async function bootstrap(prefix) {
   const app = await NestFactory.create(AppModule, { logger: false });
-  app.setGlobalPrefix(prefix);
-
-  const config = new DocumentBuilder()
-    .setTitle('Auth API Server')
-    .setDescription('Auth API for auth service')
-    .setVersion('2.0')
-    .addApiKey(
-      {
-        in: 'header',
-        name: 'x-api-key',
-        type: 'apiKey',
-      },
-      'ApiKey'
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config, {
-    extraModels: [
-      ListUsersQuery,
-      ListNamespacesQuery,
-      GetAuthorizerQuery,
-      ListCaptchasQuery,
-      ListEmailRecordsQuery,
-      ListGroupsQuery,
-      ListIndustriesQuery,
-      ListRolesQuery,
-      ListSessionsQuery,
-      ListSmsRecordsQuery,
-      ListThirdPartyQuery,
-    ],
-  });
-  const documentWithSha = {
-    hash: SHA256(JSON.stringify(document, null, 2)).toString(),
-    ...document,
-  };
-  fs.writeFileSync('./openapi.json', JSON.stringify(documentWithSha, null, 2));
-
+  if (prefix) app.setGlobalPrefix(prefix);
+  writeOpenapi(app, prefix);
   await app.close();
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-import fs from 'fs';
-
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import compression from 'compression';
-import { SHA256 } from 'crypto-js';
 import dayjs from 'dayjs';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import minMax from 'dayjs/plugin/minMax';
@@ -13,25 +9,13 @@ import * as sourceMapSupport from 'source-map-support';
 import { MongoErrorsInterceptor } from 'src/mongo';
 
 import { AppModule } from './app.module';
-import { GetAuthorizerQuery } from './auth';
-import { ListCaptchasQuery } from './captcha';
 import { AllExceptionsFilter } from './common/all-exceptions.filter';
 import { exceptionFactory } from './common/exception-factory';
 import { port, prefix } from './config/config';
-import { ListEmailRecordsQuery } from './email';
-import { ListGroupsQuery } from './group';
-import { ListIndustriesQuery } from './industry';
-import { ListNamespacesQuery } from './namespace';
-import { ListRolesQuery } from './role';
-import { ListSessionsQuery } from './session';
-import { ListSmsRecordsQuery } from './sms';
-import { ListThirdPartyQuery } from './third-party';
-import { ListUsersQuery } from './user';
+import { writeOpenapi } from './swagger';
 
 dayjs.extend(isoWeek);
 dayjs.extend(minMax);
-
-const openapiPath = `openapi.json`;
 
 async function bootstrap() {
   sourceMapSupport.install();
@@ -39,41 +23,9 @@ async function bootstrap() {
     cors: { exposedHeaders: ['Link', 'X-Total-Count'] },
   });
 
-  let swaggerPrefix = 'openapi';
   if (prefix) {
     app.setGlobalPrefix(prefix);
-    swaggerPrefix = `${prefix}/${swaggerPrefix}`;
   }
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Auth API Server')
-    .setDescription('Auth API for auth service')
-    .setVersion('2.0')
-    .addApiKey(
-      {
-        in: 'header',
-        name: 'x-api-key',
-        type: 'apiKey',
-      },
-      'ApiKey'
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig, {
-    extraModels: [
-      ListUsersQuery,
-      ListNamespacesQuery,
-      GetAuthorizerQuery,
-      ListCaptchasQuery,
-      ListEmailRecordsQuery,
-      ListGroupsQuery,
-      ListIndustriesQuery,
-      ListRolesQuery,
-      ListSessionsQuery,
-      ListSmsRecordsQuery,
-      ListThirdPartyQuery,
-    ],
-  });
-  SwaggerModule.setup(swaggerPrefix, app, document);
 
   app.use(compression());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, exceptionFactory }));
@@ -82,16 +34,10 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalInterceptors(new MongoErrorsInterceptor());
 
-  await app.listen(port);
+  // setup swagger
+  writeOpenapi(app, prefix);
 
-  // write openapi.json
-  if (process.env.NODE_ENV === 'development') {
-    const documentWithSha = {
-      hash: SHA256(JSON.stringify(document, null, 2)).toString(),
-      ...document,
-    };
-    fs.writeFileSync(openapiPath, JSON.stringify(documentWithSha, null, 2));
-  }
+  await app.listen(port);
 }
 
 bootstrap();
