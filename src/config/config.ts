dbba2cd42af38fc587769e8a30851cdeae93b20c
Wang, Tao <miffyliye@gmail.com>
2026-03-19 17:50:28 +0800

    feat: volcengine sms client (#98)
    短信发送支持火山云
    
    ### 发布建议
    
    - 上线时需要设置的环境变量或参数
    
      - `SMS_PROVIDER`: 默认aliyun，可改为volcengine
      - `VOLCENGINE_SMS_ACCOUNT`: 火山云短信消息组
      - `VOLCENGINE_SMS_ACCESS_KEY_ID`
      - `VOLCENGINE_SMS_SECRET_KEY`
    
    Co-authored-by: Wang, Tao <wangtao@36node.com>

--- src/config/config.ts
+++ src/config/config.ts
@@ -52,9 +52,15 @@ export const redis = {
 };
 
 export const sms = {
+  provider: loadEnv('SMS_PROVIDER', { default: 'aliyun' }),
   aliyun: {
     keyId: loadEnv('ALIYUN_SMS_KEY_ID'),
     keySecret: loadEnv('ALIYUN_SMS_KEY_SECRET'),
+  },
+  volcengine: {
+    account: loadEnv('VOLCENGINE_SMS_ACCOUNT'), // 消息组 ID
+    accessKeyId: loadEnv('VOLCENGINE_SMS_ACCESS_KEY_ID'),
+    secretKey: loadEnv('VOLCENGINE_SMS_SECRET_KEY'),
   },
 };
 
--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,5 +1,6 @@
 import SMSClient from '@alicloud/sms-sdk';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import sms from '@volcengine/openapi/lib/services/sms';
 
 import * as config from 'src/config';
 
@@ -7,18 +8,40 @@ import { SendSmsDto } from './dto/send-sms.dto';
 
 @Injectable()
 export class SmsService {
-  private aliyunClient: SMSClient;
-
-  constructor() {
-    this.aliyunClient = new SMSClient({
-      accessKeyId: config.sms.aliyun.keyId,
-      secretAccessKey: config.sms.aliyun.keySecret,
-    });
-  }
+  private aliyunClient?: SMSClient;
+  private volcengineClient?: sms.SmsService;
 
   async send(dto: SendSmsDto) {
+    const provider = this.getProvider();
+    switch (provider) {
+      case 'aliyun':
+        await this.sendByAliyun(dto);
+        return;
+      case 'volcengine':
+        await this.sendByVolcengine(dto);
+        return;
+      default:
+        throw new InternalServerErrorException(`Unsupported sms provider: ${provider}`);
+    }
+  }
+
+  private getProvider(): string {
+    return (config.sms.provider || 'aliyun').toLowerCase();
+  }
+
+  private getAliyunClient(): SMSClient {
+    if (!this.aliyunClient) {
+      this.aliyunClient = new SMSClient({
+        accessKeyId: config.sms.aliyun.keyId,
+        secretAccessKey: config.sms.aliyun.keySecret,
+      });
+    }
+    return this.aliyunClient;
+  }
+
+  private async sendByAliyun(dto: SendSmsDto) {
     const { phone, sign, template, params } = dto;
-    const res = await this.aliyunClient.sendSMS({
+    const res = await this.getAliyunClient().sendSMS({
       PhoneNumbers: phone,
       SignName: sign,
       TemplateCode: template,
@@ -29,6 +52,32 @@ export class SmsService {
         `Message: ${res.Message} RequestId: ${res.RequestId} BizId:${res.BizId} Code: ${res.Code}`
       );
       throw new Error(res.Message);
+    }
+  }
+
+  private getVolcengineClient(): SMSClient {
+    if (!this.volcengineClient) {
+      this.volcengineClient = new sms.SmsService();
+      this.volcengineClient.setAccessKeyId(config.sms.volcengine.accessKeyId);
+      this.volcengineClient.setSecretKey(config.sms.volcengine.secretKey);
+    }
+    return this.volcengineClient;
+  }
+
+  private async sendByVolcengine(dto: SendSmsDto) {
+    const { phone, sign, template, params } = dto;
+    const res = await this.getVolcengineClient().Send({
+      SmsAccount: config.sms.volcengine.account,
+      Sign: sign,
+      TemplateID: template,
+      PhoneNumbers: phone,
+      TemplateParam: params ? JSON.stringify(params) : undefined,
+    });
+    if (res.ResponseMetadata.Error) {
+      console.error(
+        `Message: ${res.ResponseMetadata?.Error?.Message} RequestId: ${res.ResponseMetadata?.RquestId} Service: ${res.ResponseMetadata?.Service} Code: ${res.ResponseMetadata?.Error?.Code}`
+      );
+      throw new Error(res.ResponseMetadata?.Error?.Message);
     }
   }
 }
