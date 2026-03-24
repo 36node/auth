a0eb3c44c2c56dc85893e3406cc2589ba7315468
zzswang <zzswang@gmail.com>
2026-03-24 17:46:29 +0800

    fix: thirdparty support query by tid array

--- src/third-party/dto/list-third-party.dto.ts
+++ src/third-party/dto/list-third-party.dto.ts
@@ -1,10 +1,18 @@
 import { IntersectionType, OmitType, PartialType } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
 import { QueryDto } from 'src/common';
 
 import { ThirdPartyDoc } from '../entities/third-party.entity';
 
 export class ListThirdPartyQuery extends IntersectionType(
-  PartialType(ThirdPartyDoc),
+  PartialType(OmitType(ThirdPartyDoc, ['tid'])),
   OmitType(QueryDto, ['_sort'])
-) {}
+) {
+  /**
+   * 按 tid 筛选，支持多个值
+   */
+  @IsOptional()
+  @IsString({ each: true })
+  tid?: string | string[];
+}
