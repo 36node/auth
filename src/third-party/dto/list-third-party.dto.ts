31810478df4402c3bacd11e80bd390a3daf2347f
zzswang <zzswang@gmail.com>
2026-03-28 11:30:51 +0800

    fix: list thirdparty by tid string or array

--- src/third-party/dto/list-third-party.dto.ts
+++ src/third-party/dto/list-third-party.dto.ts
@@ -2,6 +2,7 @@ import { IntersectionType, OmitType, PartialType } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 import { QueryDto } from 'src/common';
+import { ApiStringOrArray } from 'src/common/decorator';
 
 import { ThirdPartyDoc } from '../entities/third-party.entity';
 
@@ -14,5 +15,6 @@ export class ListThirdPartyQuery extends IntersectionType(
    */
   @IsOptional()
   @IsString({ each: true })
+  @ApiStringOrArray('按 tid 单个或者数组查询')
   tid?: string | string[];
 }
